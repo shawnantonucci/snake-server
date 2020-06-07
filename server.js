c50cc0ec5f904d025e3942519328364a146f9999
Shawn Antonucci <shawnantonucci2018@gmail.com>
2020-06-06 20:34:20 -0400

    validate request

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-// const path = require("path");
 const helmet = require("helmet");
 const mongoose = require("mongoose");
 
@@ -12,15 +11,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
-// app.use(express.static(path.join(__dirname, "build")));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+const validateReq = function (req, res, next) {
+  if (req.connection.remoteAddress === "https://snakenode.web.app") {
+    // if (req.connection.remoteAddress === "http://localhost:3000") {
+    next();
+  }
+  return res.status(400).json("Error...");
+};
 
 // cors origin URL - Allow inbound traffic from origin
 corsOptions = {
   origin: "https://snakenode.web.app",
+  // origin: "http://localhost:3000",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
@@ -38,7 +41,7 @@ connection.once("open", () => {
 
 const usersRouter = require("./routes/users");
 
-app.use("/users", usersRouter);
+app.use("/users", validateReq(usersRouter));
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
